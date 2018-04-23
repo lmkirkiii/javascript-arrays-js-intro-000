352b1a55e7f15d1b2c75697bc43b98a83a0ea70c
lmkirkiii@gmail.com Kirk <lmkirkiii@gmail.com>
2018-04-23 22:45:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,5 +2,10 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array,element){
   var array=[];
+  array.unshift(element);
+}
+
+function addElementToBeginningOfArray(array,element){
+  var array=[];
   array.push(element);
 }
